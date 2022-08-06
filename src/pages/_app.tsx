67cc9f42f8c8f902c3bc07c8de602645a8e7e60c
Yerkhan Baigal <baigal.erhan@gmail.com>
2022-08-06 21:06:31 +0600

    reformat

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,13 @@
-import type { AppProps } from 'next/app'
 import { MantineProvider } from '@mantine/core'
+import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-    return (
-        <MantineProvider withNormalizeCSS>
-            <Component {...pageProps} />
-        </MantineProvider>
-    )
+	return (
+		<MantineProvider withNormalizeCSS>
+			<Component {...pageProps} />
+		</MantineProvider>
+	)
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
-import type { NextPage } from 'next'
 import { Paper } from '@mantine/core'
+import type { NextPage } from 'next'
+import { CreateRoom } from '../components'
 import s from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-    return (
-        <Paper className={s.wrapper} radius={0}>
-            
-        </Paper>
-    )
+	return (
+		<Paper className={s.wrapper} radius={0}>
+			<CreateRoom />
+		</Paper>
+	)
 }
 
 export default Home
